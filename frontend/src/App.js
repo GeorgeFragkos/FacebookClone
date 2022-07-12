a4141fc8b11b1bbb273d8ae24106695fc45b78ab
fragosgeorge4@outlook.com <fragosgeorge4@outlook.com>
2022-07-12 20:25:06 +0300

    Add react router and redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Profile } from "./pages/profile";
+
 function App() {
-  return <div>welcome to frontend</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/profile" element={<Profile />} exact />
+        <Route path="/" element={<Home />} exact />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
