f954a1de52dd493100721136b8d0e600ab96a473
fragosgeorge4@outlook.com <fragosgeorge4@outlook.com>
2022-07-17 12:49:54 +0300

    Extract sepatere file for registration

---
+++ frontend/src/components/register/RegisterForm.js
@@ -0,0 +1,206 @@
+import { Form, Formik } from "formik";
+import RegisterInput from "../inputs/registerInput";
+import { useState } from "react";
+import DateOfBirthSelect from "./DateOfBirthSelect";
+import GenderSelect from "./GenderSelect";
+import DotLoader from "react-spinners/DotLoader";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import registerDataValidation from "../../validation/registerDataValidation";
+
+export default function RegisterForm({ setVisible }) {
+  const setUserInfos = {
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    bYear: new Date().getFullYear(),
+    bMonth: new Date().getMonth() + 1,
+    bDay: new Date().getDay(),
+    gender: "",
+  };
+  const [user, setUser] = useState(setUserInfos);
+  const {
+    first_name,
+    last_name,
+    email,
+    password,
+    bYear,
+    bMonth,
+    bDay,
+    gender,
+  } = user;
+
+  const [dateError, setDateError] = useState("");
+  const [genderError, setGenderError] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const registerSubmit = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_BACKEND_UR}/register`,
+        {
+          first_name,
+          last_name,
+          email,
+          password,
+          bYear,
+          bMonth,
+          bDay,
+          gender,
+        }
+      );
+      setError("");
+      setLoading(false);
+      setSuccess(data.message);
+      const { message, ...rest } = data;
+      setTimeout(() => {
+        dispatch({ type: "LOGIN", payload: rest });
+        Cookies.set("user", JSON.stringify(rest));
+        navigate("/");
+      }, 2000);
+    } catch (error) {
+      setLoading(false);
+      setSuccess("");
+      setError(error.response.data.message);
+    }
+  };
+
+  const handleRegisterChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const years = Array.from(
+    new Array(108),
+    (val, index) => setUserInfos.bYear - index
+  );
+  const months = Array.from(new Array(12), (val, index) => index + 1);
+
+  const getDays = () => {
+    return new Date(bYear, bMonth, 0).getDate();
+  };
+  const days = Array.from(new Array(getDays()), (value, index) => index + 1);
+
+  return (
+    <div className="blur">
+      <div className="register">
+        <div className="register_header">
+          <i className="exit_icon" onClick={() => setVisible(false)}></i>
+          <span>Sign Up</span>
+          <span>it's quick and easy</span>
+        </div>
+        <Formik
+          enableReinitialize
+          initialValues={{
+            first_name,
+            last_name,
+            email,
+            password,
+            bYear,
+            bMonth,
+            bDay,
+            gender,
+          }}
+          validationSchema={registerDataValidation}
+          onSubmit={() => {
+            let current_date = new Date();
+            let picked_date = new Date(bYear, bMonth - 1, bDay);
+            let atLeast14 = new Date(1970 + 14, 0, 1);
+            let noMoreThan70 = new Date(1970 + 70, 0, 1);
+            if (current_date - picked_date < atLeast14) {
+              setDateError("You are entered wrong info.");
+            } else if (current_date - picked_date > noMoreThan70) {
+              setDateError("You are entered wrong info.");
+            } else if (gender === "") {
+              setDateError("");
+              setGenderError("Please choose a gender.");
+            } else {
+              setDateError("");
+              setGenderError("");
+              registerSubmit();
+            }
+          }}
+        >
+          {(formik) => (
+            <Form className="register_form">
+              <div className="register_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="First name"
+                  name="first_name"
+                  onChange={handleRegisterChange}
+                />
+                <RegisterInput
+                  type="text"
+                  placeholder="Surname"
+                  name="last_name"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="resister_line">
+                <RegisterInput
+                  type="text"
+                  placeholder="Mobile number or email address"
+                  name="email"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="resister_line">
+                <RegisterInput
+                  type="password"
+                  placeholder="New password"
+                  name="password"
+                  onChange={handleRegisterChange}
+                />
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Date of birth <i className="info_icon"></i>
+                </div>
+                <DateOfBirthSelect
+                  bDay={bDay}
+                  bMonth={bMonth}
+                  bYear={bYear}
+                  days={days}
+                  months={months}
+                  years={years}
+                  handleRegisterChange={handleRegisterChange}
+                  dateError={dateError}
+                />
+              </div>
+              <div className="reg_col">
+                <div className="reg_line_header">
+                  Gender <i className="info_icon"></i>
+                </div>
+                <GenderSelect
+                  handleRegisterChange={handleRegisterChange}
+                  genderError={genderError}
+                />
+              </div>
+              <div className="reg_infos">
+                By clicking Sign Up , you agree to our{" "}
+                <span>Terms, Data Policy &nbsp;</span>
+                and <span>Cookie Policy.</span> You must receive SMS
+                notifications from us and can opt out at any time.
+              </div>
+              <div className="reg_btn_wrapper">
+                <button className="blue_btn open_signup">Sign Up</button>
+              </div>
+              <DotLoader color="#1876f2" loading={loading} size={30} />
+              {error && <div className="error_text">{error}</div>}
+              {success && <div className="success_text">{success}</div>}
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import { LoginForm } from "../../components/login/LoginForm";
 import Footer from "../../components/login/Footer";
-import RegisterForm from "../../components/login/RegisterForm";
+import RegisterForm from "../../components/register/RegisterForm";
 import { useState } from "react";
 
 export const Login = () => {
