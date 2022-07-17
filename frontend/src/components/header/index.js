1c13ae3d1c2c9c9e7ec5431ba891b0ae13301676
fragosgeorge4@outlook.com <fragosgeorge4@outlook.com>
2022-07-17 19:12:45 +0300

    Adding header to home page

---
+++ frontend/src/components/header/index.js
@@ -0,0 +1,78 @@
+import "./style.css";
+import { Link } from "react-router-dom";
+import {
+  Friends,
+  Gaming,
+  HomeActive,
+  Logo,
+  Market,
+  Search,
+  Watch,
+  Menu,
+  Messenger,
+  Notifications,
+  ArrowDown,
+} from "../../svg";
+import { useSelector } from "react-redux";
+
+export default function Header() {
+  const { user } = useSelector((user) => ({ ...user }));
+  console.log(user);
+  const color = "#65676b";
+  return (
+    <header>
+      <div className="header_left">
+        <Link to="/" className="header_logo">
+          <div className="circle">
+            <Logo />
+          </div>
+        </Link>
+        <div className="search search1">
+          <Search color={color} />
+          <input
+            type="text"
+            placeholder="Search Facebook"
+            className="hide_input"
+          />
+        </div>
+      </div>
+      <div className="header_middle">
+        <Link to="/" className="middle_icon active">
+          <HomeActive />
+        </Link>
+        <Link to="/" className="middle_icon hover1">
+          <Friends color={color} />
+        </Link>
+        <Link to="/" className="middle_icon hover1">
+          <Watch color={color} />
+          <div className="middle_notification">9+</div>
+        </Link>
+        <Link to="/" className="middle_icon hover1">
+          <Market color={color} />
+        </Link>
+        <Link to="/" className="middle_icon hover1">
+          <Gaming color={color} />
+        </Link>
+      </div>
+      <div className="header_right">
+        <Link to="/profile" className="profile_link hover1">
+          <img src={user?.picture} />
+          <span>{user?.first_name}</span>
+        </Link>
+        <div className="circle_icon hover1">
+          <Menu />
+        </div>
+        <div className="circle_icon hover1">
+          <Messenger />
+        </div>
+        <div className="circle_icon hover1">
+          <Notifications />
+          <div className="right_notification">5</div>
+        </div>
+        <div className="circle_icon hover1">
+          <ArrowDown />
+        </div>
+      </div>
+    </header>
+  );
+}
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,3 +1,9 @@
+import Header from "../../components/header";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
