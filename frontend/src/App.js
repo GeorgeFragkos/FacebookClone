cfd3cfea1492f295cc2b366d57669f9b96d796cc
fragosgeorge4@outlook.com <fragosgeorge4@outlook.com>
2022-08-03 01:11:26 +0300

    Adding stories and make home page responsive

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home";
+import Home from "./pages/home";
 import { Login } from "./pages/login";
 import { Profile } from "./pages/profile";
 import LoggedInRoutes from "./routes/LoggedInRoutes";
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,15 +1,22 @@
+import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import CreatePost from "../../components/createPost";
 import Header from "../../components/header";
 import LeftHome from "../../components/home/left";
 import RightHome from "../../components/home/right";
-
-export const Home = () => {
-  const { user } = useSelector((state) => ({ ...state }));
+import Stories from "../../components/home/stories";
+import "./style.css";
+export default function Home() {
+  const { user } = useSelector((user) => ({ ...user }));
   return (
-    <div>
+    <div className="home">
       <Header />
       <LeftHome user={user} />
+      <div className="home_middle">
+        <Stories />
+        <CreatePost user={user} />
+      </div>
       <RightHome user={user} />
     </div>
   );
-};
+}
