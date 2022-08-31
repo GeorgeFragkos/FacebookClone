9555b15b70a14bae6701a14ddceb016625442042
GEORGE FRAGKOS <fragkosgeorge4@gmail.com>
2022-08-31 19:54:03 +0300

    Add post route

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -8,3 +8,14 @@ exports.createPost = async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 };
+
+exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find()
+      .populate("user", "first_name last_name picture username gender")
+      .sort({ createdAt: "desc" });
+    res.json(posts);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
-const { createPost } = require("../controllers/post");
+const { createPost, getAllPosts } = require("../controllers/post");
 const { authUser } = require("../middlwares/auth");
 
 const router = express.Router();
 
 router.post("/createPost", authUser, createPost);
+router.get("/getAllPosts", authUser, getAllPosts);
 
 module.exports = router;
